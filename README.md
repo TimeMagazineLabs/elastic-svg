ebe68a1a298c86c9283a3436c0fb901f3e88f133
Chris Wilson <christopher.e.wilson@gmail.com>
2015-06-13 19:26:15 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# elastic-svg
-Responsive SVG elements that behave like divs and resize automatically (if you want)
+# Elastic SVG elements
+=======
+
+Scalable Vector Graphics are one of the Web's most triumphant technologies, but they're not as responsive as they might be. This is a very lightweight, dependency-free library that generates `svg` elements that fit to their container and scale or don't scale as necessary.
+
+## Example
+
+	var elasticSVG = require("elastic-svg");
+
+	var b = elasticSVG("#container", {
+		height: 400
+	});
+
+	var b = elasticSVG("#container", {
+		aspect: 0.5
+	});
+
+## Options
+You must pass `base` a selector representing the parent element in width the SVG will be created. You can optionally pass an object as a second argument representing options. These are:
+
+| property | description |
+| -------- | ----------- |
+| `width`  | The initial width of the SVG. Defaults to width of parent |
+| `height` | The initial height of the SVG. Defaults to the aspect ratio. |
+| `aspect` | The aspect ratio of the SVG. Defaults to 0.618, approximately the [golden ratio](http://en.wikipedia.org/wiki/Golden_ratio) |
+| `resize` | Info about how the SVG should scale. Current options are: <ul><li>`auto`: Set the `viewBox` to the initial width and height, thus scaling automatically according to the SVG specification </li></ul> |
+| `onResize` | callback function to fire whenever the SVG resizes. This function will receive three arguments: the (new) width, height, and scale, which is (current width) / (original width) |
+
+## A callback example
+
+	var elasticSVG = require("elastic-svg");
+
+	var b = base("#container", {
+		onResize: function(w, h, s) {
+			console.log("Scale is " + s);
+		}
+	});
---
+++ index.js
@@ -0,0 +1,106 @@
+(function() {
+    var root = this;
+
+	// create a new SVG element
+	var elasticSVG = function(selector, opts) {
+		opts = opts || {};
+
+		// containing DOM element, which defaults to body
+		var parent = document.querySelectorAll(selector || "body");
+
+		if (!parent || !parent.length) {
+			console.log("Couldn't find a parent for elasticSVG making the selector '" + selector + "'");
+			return;
+		}
+
+		parent = parent[0];
+
+		// you can specify a width if you like, or we'll snap to size of container
+		var base = {
+			width: typeof opts.width !== "undefined" ? opts.width : parent.clientWidth,
+			scale: 1
+		};
+
+		// we need to remember the original width for scaling purposes
+		base.original_width = base.width;
+
+		// you can either specify the height or the aspect ratio. If neither is specified, refaults to roughly the golden ratio
+	    if (typeof opts.height !== "undefined") {
+	    	base.height = opts.height;
+	    	opts.aspect = base.height / base.width;
+	    } else {
+		    opts.aspect = typeof opts.aspect !== "undefined" ? opts.aspect : 0.618;
+			base.height = base.width * opts.aspect;
+	    }
+
+        var xmlns = "http://www.w3.org/2000/svg";
+        var svg = document.createElementNS(xmlns, "svg");
+		svg.setAttributeNS(null, "width", base.width);
+		svg.setAttributeNS(null, "height", base.height);
+		parent.appendChild(svg);
+		
+		// setting resize to "auto" sets the viewport to the original width and height so that the SVG always scales
+	    if (opts.resize && opts.resize == "auto") {
+			svg.setAttributeNS(null, "viewBox", "0 0 " + base.width + " " + base.height);
+	    }
+		
+		function resize() { 
+			console.log(parent)
+			base.width = parent.clientWidth;
+		    base.height = base.width * opts.aspect;
+		    base.scale = base.width / base.original_width;
+
+			svg.setAttributeNS(null, "width", base.width);
+			svg.setAttributeNS(null, "height", base.height);
+
+			// optional callback
+			if (opts.onResize) {
+				opts.onResize(base.width, base.height, base.scale);
+			}
+		}
+
+		var resizeTimer;
+
+		// http://stackoverflow.com/questions/3339825/what-is-the-best-practise-to-not-to-override-other-bound-functions-to-window-onr
+		function addResizeEvent(func, dur) {
+		    var oldResize = window.onresize,
+				resizeTimer,
+				dur = typeof dur === "undefined" ? 250 : parseInt(dur, 10);
+
+			window.onresize = function () {
+				clearTimeout(resizeTimer);
+
+		        if (typeof oldResize === 'function') {
+		            oldResize();
+		        }
+
+				resizeTimer = setTimeout(function() {
+					func();
+				}, dur);
+			}
+		}
+
+		addResizeEvent(resize, 250);
+
+		if (opts.resize && opts.resize === "auto") {
+			resize(); // call this on load since sometimes the initial conditions are wider than container
+		}
+
+		base.setResize = function(f) {
+			opts.onResize = f;
+		}
+
+		base.svg = svg;
+		return base;
+	}
+
+	// support various modular environments
+	if (typeof define === "function" && define.amd) { // RequireJS
+	    define(elasticSVG);
+	} else if (typeof module === "object" && module.exports) { // browserify
+	    module.exports = elasticSVG;
+	} else {
+	    root.elasticSVG = elasticSVG; // directly included
+	}
+
+}());
