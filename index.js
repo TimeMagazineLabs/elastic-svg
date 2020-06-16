306da30149bdbb36e1b823ec84e8fcdda0ba97b2
Chris Wilson <christopher.e.wilson@gmail.com>
2020-06-16 14:39:03 -0400

    Fixing some logic with resizing

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 export default function elasticSVG(selector, opts) {
 	opts = opts || {};
 
+	let hasWidth = opts.hasOwnProperty("width");
+	let hasHeight = opts.hasOwnProperty("height");
+	let hasAspect = opts.hasOwnProperty("aspect");
+
 	// containing DOM element, which defaults to body
 	var parent = document.querySelectorAll(selector || "body");
 
@@ -13,25 +17,47 @@ export default function elasticSVG(selector, opts) {
 
 	parent = parent[0];
 
+	let initialWidth = parent.clientWidth;
+
 	// you can specify a width if you like, or we'll snap to size of container
 	var base = {
-		width: typeof opts.width !== "undefined" ? opts.width : parent.clientWidth,
+		width: null,
+		height: null,
+		aspect: null,
 		scale: 1
 	};
 
 	// we need to remember the original width for scaling purposes
-	base.original_width = base.width;
 
-	// you can either specify the height or the aspect ratio. If neither is specified, refaults to roughly the golden ratio
+	// you can either specify the height and width, which will computed the aspect ration, or one of these and the aspect ratio.
+	// If neither is specified, defaults to roughly the golden ratio
 	// specifying the height keeps the svg at a standard height and only resizes the width
 	// specifying the aspect ratio resizes both
-    if (typeof opts.height !== "undefined") {
-    	base.height = opts.height;
-    	base.aspect = base.height / base.width;
-    } else {
-	    opts.aspect = typeof opts.aspect !== "undefined" ? opts.aspect : 0.618;
-		base.height = base.width * opts.aspect;
-    }
+
+	if (hasWidth && hasHeight) {
+		if (hasAspect) {
+			console.log("Overriding aspect ratio since both `width` and `height` are specified");
+			opts.aspect = opts.height / opts.width;
+		}
+		base.width = opts.width; 
+		base.height = opts.height; 
+	} else { // if has neither an aspect or BOTH a width and height, default to approximately the golden ratio
+	    opts.aspect = opts.aspect || 0.618;
+
+		if (hasWidth && !hasHeight) {
+			base.width = opts.width; 
+			base.height = base.width * opts.aspect;
+		} else if (hasHeight) {
+			base.height = opts.height;
+			base.width = base.height / opts.aspect;
+		} else { // if only has aspect
+			base.width = initialWidth;
+			base.height = base.width * opts.aspect;
+		}
+	}
+
+	base.original_width = base.width;
+
 
 	// create a new SVG element
     var xmlns = "http://www.w3.org/2000/svg";
@@ -47,15 +73,20 @@ export default function elasticSVG(selector, opts) {
 
 	// function called when the window resizes		
 	function resize() { 
+		let hasWidth = opts.hasOwnProperty("width");
+		let hasHeight = opts.hasOwnProperty("height");
+		let hasAspect = opts.hasOwnProperty("aspect");
+
 		base.width = parent.clientWidth;
 		svg.setAttributeNS(null, "width", base.width);
-
-		// only resize the height if aspect was specified instead of height
-		if (opts.aspect) {
-		    base.height = base.width * opts.aspect;
-			svg.setAttributeNS(null, "height", base.height);
+		
+		// if `height` is not specified, resize it to the aspect ratio
+		if (!hasHeight) {
+			base.height = base.width * opts.aspect;
+			svg.setAttributeNS(null, "height", base.height);				
 		}
 
+		base.aspect = base.height / base.width;
 	    base.scale = base.width / base.original_width;
 
 		// optional callback
@@ -68,7 +99,10 @@ export default function elasticSVG(selector, opts) {
 		resize();
 	});
 
-	resize(); // call this on load since sometimes the initial conditions are wider than container
+	// manually trigger a page resize and run the resize function once
+	window.dispatchEvent(new Event('resize'));
+
+	// resize(); // call this on load since sometimes the initial conditions are wider than container
 
 	// methods
 	base.setResize = function(f) {
